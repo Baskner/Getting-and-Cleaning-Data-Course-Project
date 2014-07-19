2c186258c61100f73e45d084b23507eab4255363
Baskner <ralfkoerbner@hotmail.com>
2014-07-19 15:36:46 +0200

    first commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 
 library(data.table)
+
 feat <- fread("UCI HAR Dataset/features.txt")$V2
 act <- fread("UCI HAR Dataset/activity_labels.txt")
 
@@ -23,17 +24,20 @@ dtrain <- cbind(trains, train, trainy)
 d <- rbind(dtest, dtrain)
 
 cols <- names(d)
-col <- grep("(-(mean|std)\\(\\)|V1|subject)", cols, value=T)
+cols <- grep("(-(mean|std)\\(\\)|V1|subject)", cols, value=T)
 
-d <- d[, col]
+d <- d[, cols]
 
 dat <- merge(d, act)
-dat$V1 <- null
+dat$V1 <- NULL
 
 tidy <- aggregate(. ~ activity + subject, data=dat, mean)
+
 str(tidy)
 summary(tidy)
 head(tidy)
+
+write.table(tidy, file="tidy.txt")
 
 library(ggplot2)
 dat$activity <- as.factor(dat$activity)
